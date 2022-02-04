028fd94070d9f1ca95a362adaf052b6636abc19a
dilpath <dilan.private+github@outlook.com>
2022-02-04 14:39:19 +0100

    black setup

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import os
 import re
 
 
-org = 'ICB-DCM'
-repo = 'fiddy'
+org = "ICB-DCM"
+repo = "fiddy"
 
 
 def read(fname):
@@ -16,13 +16,11 @@ def read(fname):
 def absolute_links(txt):
     """Replace relative links by absolute links, on GitHub."""
 
-    raw_base = \
-        f"(https://raw.githubusercontent.com/{org}/{repo}/main/"
-    embedded_base = \
-        f"(https://github.com/{org}/{repo}/tree/main/"
+    raw_base = f"(https://raw.githubusercontent.com/{org}/{repo}/main/"
+    embedded_base = f"(https://github.com/{org}/{repo}/tree/main/"
     # iterate over links
-    for var in re.findall(r'\[.*?\]\((?!http).*?\)', txt):
-        if re.match(r'.*?.(png|svg)\)', var):
+    for var in re.findall(r"\[.*?\]\((?!http).*?\)", txt):
+        if re.match(r".*?.(png|svg)\)", var):
             # link to raw file
             rep = var.replace("(", raw_base)
         else:
@@ -32,13 +30,13 @@ def absolute_links(txt):
     return txt
 
 
-minimum_python_version = '3.8.0'  # for NumPy
-if sys.version_info < tuple(map(int, minimum_python_version.split('.'))):
-    sys.exit(f'{repo} requires Python >= {minimum_python_version}')
+minimum_python_version = "3.8.0"  # for NumPy
+if sys.version_info < tuple(map(int, minimum_python_version.split("."))):
+    sys.exit(f"{repo} requires Python >= {minimum_python_version}")
 
 # read version from file
-__version__ = ''
-version_file = os.path.join(repo, 'version.py')
+__version__ = ""
+version_file = os.path.join(repo, "version.py")
 # sets __version__
 exec(read(version_file))  # pylint: disable=W0122 # nosec
 
@@ -47,37 +45,37 @@ exec(read(version_file))  # pylint: disable=W0122 # nosec
 setup(
     name=repo,
     version=__version__,
-    description='Finite difference methods.',
-    long_description=absolute_links(read('README.md')),
+    description="Finite difference methods.",
+    long_description=absolute_links(read("README.md")),
     long_description_content_type="text/markdown",
-    url=f'https://github.com/{org}/{repo}',
-    packages=find_packages(exclude=['doc*', 'test*']),
+    url=f"https://github.com/{org}/{repo}",
+    packages=find_packages(exclude=["doc*", "test*"]),
     install_requires=[
-        'joblib',
-        'numpy',
-        'pandas',
+        "joblib",
+        "numpy",
+        "pandas",
     ],
     include_package_data=True,
-    python_requires=f'>={minimum_python_version}',
+    python_requires=f">={minimum_python_version}",
     extras_require={
-        'tests': [
-            'pytest',
-            'sympy',
-            'more-itertools',
+        "tests": [
+            "pytest",
+            "sympy",
+            "more-itertools",
         ],
-        'amici': [
-            'amici',
-            'petab',
+        "amici": [
+            "amici",
+            "petab",
         ],
-        'pypesto': [
+        "pypesto": [
             # TODO setup.cfg with dependency on amici, petab
-            'pypesto',
+            "pypesto",
         ],
-        'doc': [
-            'sphinx',
-            'sphinx_rtd_theme',
-            'sphinx-autodoc-typehints',
-            'mock',
-        ]
-    }
+        "doc": [
+            "sphinx",
+            "sphinx_rtd_theme",
+            "sphinx-autodoc-typehints",
+            "mock",
+        ],
+    },
 )
