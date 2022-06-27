amici: fix tests; amici derivative output now matches fiddy derivative structure; clean

fiddy/constants.py
@@ -12,6 +12,7 @@ __all__ = [
 
 
 class Type:
+    """Type annotation variables."""
     # The size is applied as the Euclidean distance in the
     # specified direction, between the point and the stepped point
     SCALAR = np.float64
@@ -37,6 +38,7 @@ class Type:
 # FIXME rename, since this can be the name of the base class in `derivative.py`
 # FIXME use Difference instead, then i.e. Extrapolation too
 class MethodId(str, Enum):
+    """Finite different method IDs."""
     BACKWARD = "backward"
     CENTRAL = "central"
     FORWARD = "forward"
@@ -44,19 +46,6 @@ class MethodId(str, Enum):
     # five point?
     # TODO separate enum for "order" of method?
     #      e.g. for higher-order derivatives?
-
-
-#class Difference(str, Enum):
-#    BACKWARD = MethodId.BACKWARD
-#    CENTRAL = MethodId.CENTRAL
-#    FORWARD = MethodId.FORWARD
-
-
-## necessary? same as MethodId?
-#class GradientCheckMethod(str, Enum):
-#    BACKWARD = Difference.BACKWARD
-#    CENTRAL = Difference.CENTRAL
-#    FORWARD = Difference.FORWARD
 
 
 #class AnalysisMethod(str, Enum):

fiddy/derivative_check.py
@@ -107,7 +107,7 @@ class DerivativeCheck(abc.ABC):
 class NumpyIsCloseDerivativeCheck(DerivativeCheck):
     method_id = 'np.isclose'
 
-    def method(self, *args, rtol: float = 1e-2, atol: float = 1e-15, **kwargs):
+    def method(self, *args, **kwargs):
         directional_derivative_check_results = []
         success = True
         for direction_index, directional_derivative in enumerate(self.derivative.directional_derivatives):
@@ -123,8 +123,6 @@ class NumpyIsCloseDerivativeCheck(DerivativeCheck):
                 test_value,
                 expected_value,
                 *args,
-                rtol=rtol,
-                atol=atol,
                 **kwargs,
             )
 

fiddy/extensions/amici/amici.py
@@ -1,6 +1,6 @@
 from functools import partial
 from inspect import signature
-from typing import Any, Callable, Dict, List, Tuple, Union
+from typing import Any, Callable, Dict, List, Sequence, Tuple, Union
 
 import numpy as np
 import amici
@@ -136,6 +136,8 @@ def run_amici_simulation_to_cached_functions(
     Args:
         derivative_variables:
             The variables that derivatives will be computed or approximated for. See the keys of `all_rdata_derivatives` for options.
+        parameter_ids:
+            The IDs that correspond to the values in the parameter vector that is simulated.
 
     tmp:
         derivative_shapes = None
@@ -199,13 +201,21 @@ def run_amici_simulation_to_cached_functions(
         # np.concatenate([rdata.x, rdata.y])
         return rdata_flat
 
-    def derivative(point: Type.POINT):
+    def derivative(point: Type.POINT, return_dict: bool = False):
         rdata = run_amici_simulation(point=point)
         outputs = {
             variable: rdata_array_transpose(array=fiddy_array(getattr(rdata, derivative_variable)), variable=derivative_variable)
             for variable, derivative_variable in chosen_derivatives.items()
         }
-        rdata_flat = np.concatenate([output.flat for output in outputs.values()])
+        rdata_flat = np.concatenate(
+            [
+                output_array.reshape(-1, output_array.shape[-1])
+                for output_array in outputs.values()
+            ],
+            axis=0,
+        )
+        if return_dict:
+            return outputs
         return rdata_flat
 
     #def function(point: Type.POINT):

tests/extensions/amici/test_amici.py
@@ -21,6 +21,7 @@ from fiddy.extensions.amici import (
     simulate_petab_to_cached_functions,
     reshape,
     flatten,
+    default_derivatives,
 )
 
 
@@ -69,6 +70,7 @@ def test_run_amici_simulation_to_functions(problem_generator):
     amici_solver.setSensitivityOrder(amici.SensitivityOrder_first)
 
     parameter_ids = list(petab_problem.parameter_df[petab_problem.parameter_df.estimate == 1].index)
+    parameter_indices = [amici_model.getParameterIds().index(parameter_id) for parameter_id in parameter_ids]
 
     (
         amici_function,
@@ -81,13 +83,7 @@ def test_run_amici_simulation_to_functions(problem_generator):
         amici_solver=amici_solver,
     )
 
-    #f = amici_function(point)
-    #d = amici_derivative(point)
-    #breakpoint()
-    #expected_derivative = amici_derivative(point)
-
-    #parameter_ids = list(petab_problem.parameter_df[petab_problem.parameter_df.estimate == 1].index)
-    #parameter_scales = dict(petab_problem.parameter_df[petab_problem.parameter_df.estimate == 1].parameterScale)
+    expected_derivative = amici_derivative(point)[..., parameter_indices]
 
     derivative = get_derivative(
         function=amici_function,
@@ -101,16 +97,18 @@ def test_run_amici_simulation_to_functions(problem_generator):
         #],
         success_checker=Consistency(),
     )
-    test_value = derivative.value
-    #expected_value = expected_derivative_function(point)
-    breakpoint()
+    test_derivative = derivative.value
 
+    # The test derivative is close to the expected derivative.
+    assert np.isclose(test_derivative, expected_derivative, rtol=1e-1, equal_nan=True).all()
+
+    # Same as above assert.
     check = NumpyIsCloseDerivativeCheck(
         derivative=derivative,
         expectation=expected_derivative,
         point=point,
     )
-    result = check()
+    result = check(rtol=1e-1, equal_nan=True)
     assert result.success
 
 
@@ -153,115 +151,5 @@ def test_simulate_petab_to_functions(problem_generator):
         expectation=expected_derivative,
         point=point,
     )
-    result = check()
+    result = check(rtol=1e-2)
     assert result.success
-
-    """
-    derivative_check_partial = partial(
-        fiddy.gradient_check,
-        function=function,
-        point=point,
-        gradient=gradient,
-        sizes=[
-            1e-1,
-            1e-2,
-            1e-3,
-            1e-4,
-            1e-5,
-            1e-6,
-            1e-7,
-            1e-8,
-            1e-9,
-            1e-10,
-            1e-11,
-            1e-12,
-            1e-13,
-        ],
-    )
-
-    success_forward, results_df_forward = gradient_check_partial(
-        fd_gradient_method="forward",
-    )
-    success_backward, results_df_backward = gradient_check_partial(
-        fd_gradient_method="backward",
-    )
-    success_central, results_df_central = gradient_check_partial(
-        fd_gradient_method="central",
-    )
-
-    # All gradient checks were successful.
-    assert success_forward
-    assert success_backward
-    assert success_central
-
-    # The test gradient is close to the expected gradient for both parameters
-    # and all methods.
-    # Only one result is returned for each dimension.
-    dimensions = [i for i in range(len(point))]
-    rel_tol = 1e-1
-    results_dfs = [
-        results_df_forward,
-        results_df_backward,
-        results_df_central,
-    ]
-    for results_df in results_dfs:
-        for dimension in dimensions:
-            best_test_index = (
-                (
-                    results_df.loc[
-                        results_df["dimension"] == dimension, "test_gradient"
-                    ]
-                    - expected_gradient[dimension]
-                )
-                .abs()
-                .idxmin()
-            )
-            best_test_gradient = results_df.iloc[best_test_index][
-                "test_gradient"
-            ]
-            assert math.isclose(
-                best_test_gradient,
-                expected_gradient[dimension],
-                rel_tol=rel_tol,
-            )
-
-    # Errors with central method are far lower than errors with forward or
-    # backward methods.
-    errors = ["|aerr|", "|rerr|"]
-    results_dfs = [
-        results_df_forward,
-        results_df_backward,
-    ]
-    for results_df in results_dfs:
-        for dimension in dimensions:
-            # Sufficent for this test to pick a single size, even though
-            # different sizes may be better for different errors.
-            # So, test at the minimum absolute error.
-            best_test_index = (
-                (
-                    results_df.loc[
-                        results_df["dimension"] == dimension, "test_gradient"
-                    ]
-                    - expected_gradient[dimension]
-                )
-                .abs()
-                .idxmin()
-            )
-            best_test_index_central = (
-                (
-                    results_df_central.loc[
-                        results_df_central["dimension"] == dimension,
-                        "test_gradient",
-                    ]
-                    - expected_gradient[dimension]
-                )
-                .abs()
-                .idxmin()
-            )
-            for error in errors:
-                assert (
-                    results_df.iloc[best_test_index][error]
-                    > 5
-                    * results_df_central.iloc[best_test_index_central][error]
-                )
-    """