09221392698b8c41fe80b9d9a9d51dfe70b900ed
Stephan Grein <stephan.grein@uni-bonn.de>
2023-11-13 09:57:50 +0100

    Fix check?

--- fiddy/derivative_check.py
+++ fiddy/derivative_check.py
@@ -216,7 +216,9 @@ class HybridDerivativeCheck(DerivativeCheck):
                                 abs(grad_mean - fd_mean)
                                 / abs(fd_range + np.finfo(float).eps)
                             ) > kwargs["rtol"]:
-                        results.append(False)
+                                results.append(False)
+                            else:
+                                results.append(True)
                         else:
                             results.append(
                                 None
